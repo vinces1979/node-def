631494dc31b434a75f6340352756722e1e66f04f
Vince Spicer <vince@vinces.ca>
2019-08-01 05:37:20 -0400

    add example this error

--- examples/example.js
+++ examples/example.js
@@ -10,16 +10,31 @@ myFunc(100)
 myFunc(null, {c:true})
 
 const foo = def('a1', 'a2', {o:false, server:null, timeout: 1000}, function(args){
-
     console.log(args)
+})
 
-
-}, {compact:true})
-
-foo()
+foo({a1: 'yeah', server:'github'})
 
 foo({a1: 100})
 
 foo(200)
+
+const foobar = def('a', 'b', {owner:false, ttl:1000, timeout: 5000}, function(args){
+    console.log(args)
+})
+
+foobar(1,2,34,5)
+
+foobar(1,2,{ttl: 50000})
+
+
+const foob = def('a', 'b', {owner:false, ttl:1000, timeout: 5000}, function(args){
+    console.log(args)
+}, {compact: true});
+
+foob(1,2,34,5)
+
+foob(1,2,{ttl: 50000})
+
 
 const bar = def('a')
